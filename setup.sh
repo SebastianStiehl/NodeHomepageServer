b633c7e519d7576f0639ff0720b6897f71e8aaea
SebastianStiehl <sebastian@stiehl.io>
2016-01-22 14:53:31 +0100

    update before install

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo yum update -y
+sudo apt-get -y update
 curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y git
